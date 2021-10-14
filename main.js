51e4932bc89c79201fc4928a07a90d378be01057
gooooooooooooogle <1185025336@qq.com>
2021-10-14 17:39:57 +0800

    Init

---
+++ main.js
@@ -0,0 +1,32 @@
+const axios = require('axios')
+const fs = require('fs')
+
+function main () {
+	let url = 'http://feeds.feedburner.com/mattkaydiary/pZjG'
+	axios.get(url).then(response => {
+		const html = response.data 
+
+		const start_key = 'clash(请开启代理后再拉取)：'
+		const end_key = '</div>'
+		const index = html.indexOf(start_key)
+		const start_index = index + start_key.length
+		const content = html.substring(start_index)
+		const len = content.indexOf(end_key)
+		let path = content.slice(0, len)
+
+		console.log('path:' + path)
+		
+		if (path && path.length > 0) {
+			axios.get(path).then(response => {
+				fs.writeFile('./Clash.yaml', response, err => {
+					if (err) 
+						console.log('error', err.message) 
+					else 
+						console.log('success')	
+				})
+			})
+		}
+	})
+}
+
+main()
