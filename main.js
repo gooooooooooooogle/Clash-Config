459910c2b3b3cfe28be9ec28531fa2bce15af441
gooooooooooooogle <1185025336@qq.com>
2021-10-15 09:28:18 +0800

    Init

--- main.js
+++ main.js
@@ -5,38 +5,36 @@ function main () {
 	let url = 'http://feeds.feedburner.com/mattkaydiary/pZjG'
 	axios.get(url).then(response => {
 		const html = response.data 
+		let path = html.match(/(?<=clash\(请开启代理后再拉取\)：)(.*?)(?=&lt;\/div&gt)/g)
+		
+		console.log('路径:' + path)
+		console.log('path类型:' + typeof path)
+		if (path.length > 0) {
 
-		/*
-		const start_key = 'clash(请开启代理后再拉取)：'
-		const end_key = '</div>'
-		const index = html.indexOf(start_key)
-		const start_index = index + start_key.length
-		const content = html.substring(start_index)
-		const len = content.indexOf(end_key)
+			path = path[0].replace(/amp;/g, '')
 
-		*/
+			if (path && path.length > 0) {
+				axios.get(path).then(response => {
 
-		path = html.match(/clash\(请开启代理后再拉取\)：(.+?)<\/div>/)
+					console.log('response类型:' + typeof response)
 
-		// let path = content.slice(0, len)
-
-		console.log('Config配置文件路径:' + path)
-		console.log('Config配置文件类型:' + typeof path)
-		path = path.replace(/amp;/g, '')
-
-		if (path && path.length > 0) {
-			axios.get(path).then(response => {
-
-				console.log('response类型:' + typeof response)
-
-				fs.writeFile('./Clash.yaml', response, err => {
-					if (err) 
-						console.log('error') 
-					else 
-						console.log('success')	
+					fs.writeFile('./Clash.yaml', response, err => {
+						if (err) 
+							console.log('error') 
+						else 
+							console.log('success')	
+					})
 				})
+			}
+		} else {
+			fs.writeFile('./Clash.yaml', '', err => {
+				if (err) 
+					console.log('write null error') 
+				else 
+					console.log('write null success')	
 			})
 		}
+		
 	})
 }
 
